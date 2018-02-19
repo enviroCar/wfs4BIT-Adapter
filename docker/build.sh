fad1ed6ce680b6852afc49f87a3213859725e7f3
dewall <a.dewall@52north.org>
2018-02-19 10:29:09 +0100

    updated docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
+source secret.sh
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${scriptDir}"
 
+# create docker-compose files
+if [ -f docker-compose.yml ]; then
+    rm docker-compose.yml
+    rm Dockerfile
+fi
 
+# replace secrets in docker related files
+sed -e "s/__PROVIDER__/$PROVIDER_ID/g; s/__SECRET__/${SECRET_KEY}/g" template/docker-compose.yml >> docker-compose.yml
+cp ./template/Dockerfile Dockerfile
+
+# clean maven build.
 cd ../ && mvn clean install
 
+# build the docker image
 docker build -t envirocar/wfs4bit \
     --build-arg JAR_FILE=$(find ./target/ -name "*.jar") \
     -f docker/Dockerfile .
